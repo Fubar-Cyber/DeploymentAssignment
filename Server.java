0401d1a5c52b1af6df96384435b2f2a24382f923
jensy <116689881+Fubar-Cyber@users.noreply.github.com>
2023-04-23 19:22:56 -0400

    Add files via upload
    A client/server application with two parts, a server, and a client. Have the client send the server a request to compute whether the number that the user provided is prime. The server responds with true or false, then the client displays the answer.

---
+++ Server.java
@@ -0,0 +1,80 @@
+package application;
+
+import java.io.*;
+import java.net.*;
+import java.util.Date;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+public class Server extends Application {
+  @Override // Override the start method in the Application class
+  public void start(Stage primaryStage) {
+    // Text area for displaying contents
+    TextArea ta = new TextArea();
+
+    // Create a scene and place it in the stage
+    Scene scene = new Scene(new ScrollPane(ta), 450, 200);
+    primaryStage.setTitle("Server"); // Set the stage title
+    primaryStage.setScene(scene); // Place the scene in the stage
+    primaryStage.show(); // Display the stage
+    
+    new Thread( () -> {
+      try {
+        // Create a server socket
+        ServerSocket serverSocket = new ServerSocket(8000);
+        Platform.runLater(() ->
+          ta.appendText("Server started at " + new Date() + '\n'));
+  
+        while (true) {
+          // Listen for a connection request
+          Socket socket = serverSocket.accept();
+  
+          // Create data input and output streams
+          DataInputStream inputFromClient = new DataInputStream(
+            socket.getInputStream());
+          DataOutputStream outputToClient = new DataOutputStream(
+            socket.getOutputStream());
+  
+          // Receive number from the client
+          int number = inputFromClient.readInt();
+  
+          // Check if the number is prime
+          boolean isPrime = true;
+          for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+              isPrime = false;
+              break;
+            }
+          }
+  
+          // Send the result back to the client
+          if (isPrime) {
+            outputToClient.writeBoolean(true);
+            Platform.runLater(() ->
+              ta.appendText("The number " + number + " is a prime number.\n"));
+          } else {
+            outputToClient.writeBoolean(false);
+            Platform.runLater(() ->
+              ta.appendText("The number " + number + " is not a prime number.\n"));
+          }
+        }
+      }
+      catch(IOException ex) {
+        ex.printStackTrace();
+      }
+    }).start();
+  }
+
+  /**
+   * The main method is only needed for the IDE with limited
+   * JavaFX support. Not needed for running from the command line.
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
+
